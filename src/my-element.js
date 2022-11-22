e2961242fc43281b1a5c45400ea48690a1fb7840
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-11-22 14:08:07 +0300

    Created delete element

--- src/my-element.js
+++ src/my-element.js
@@ -12,13 +12,30 @@ import { LitElement, html } from 'lit';
 class MyElement extends LitElement {
   render() {
     return html`
-    
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
       <div class="card">
-        <div class="circle">2</div>
-        <h1 textcolor="blue">View Two</h1>
-        <p>Ea duis bonorum nec, falli paulo aliquid ei eum.</p>
-        <p>Id nam odio natum malorum, tibique copiosae expetenda mel ea.Detracto suavitate repudiandae no eum. Id adhuc minim soluta nam.Id nam odio natum malorum, tibique copiosae expetenda mel ea.</p>
-      </div>
+      <div class="block">
+  <span class="tag is-success">
+    Welcome to Intertel
+    <button class="delete is-small"  onClick="onDeleteAlert1()"></button>
+  </span>
+</div>
+
+<div class="notification is-danger">
+  <button class="delete"  onClick="onDeleteAlert2()"></button>
+  This is your number one provider of IT solutions
+</div>
+
+<article class="message is-info">
+  <div class="message-header">
+    Info
+    <button class="delete"  onClick="onDeleteAlert3()"></button>
+  </div>
+  <div class="message-body">
+    InterTel provide value for your money
+  </div>
+</article>
       
     `;
   }
